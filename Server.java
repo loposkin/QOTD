60c1ea06d78a1856b3b6ba1d7ed91c733f0a5bbe
loposkin <loposkin@gmail.com>
2019-02-22 20:08:05 +0100

    some improvements and qotd.txt added

--- Server.java
+++ Server.java
@@ -12,11 +12,16 @@ import java.util.Random;
 
 public class Server {
     private ServerSocket serverSocket;
+
     public Server(int port) throws IOException{
         this.serverSocket = new ServerSocket(port);
         this.serverSocket.setSoTimeout(10000);
     }
 
+    /**
+     * accepts connection on listening port
+     * @return socket of connected client
+     */
     private Socket acceptConnection() {
         Socket clientSocket = null;
         try {
@@ -29,30 +34,11 @@ public class Server {
         return clientSocket;
     }
 
-    public static void main(String[] args) throws IOException {
-        int port = Integer.valueOf(args[0]);
 
-        Server server = new Server(port);
-
-        List<String> quotes = readFile();
-        Random random = new Random();
-
-        while (true) {
-            Socket clientSocket = server.acceptConnection();
-
-            DataOutputStream outputStream = null;
-            if(clientSocket != null) {
-                outputStream = new DataOutputStream(clientSocket.getOutputStream());
-            }
-            outputStream.writeUTF(quotes.get(random.nextInt(quotes.size())));
-            if(clientSocket != null) {
-                clientSocket.close();
-            }
-        }
-
-
-    }
-
+    /**
+     * reads file from current directory
+     * @return
+     */
     private static List<String> readFile() {
         ArrayList<String> arrayList = new ArrayList<String>();
         Path path = Paths.get("qotd.txt");
@@ -67,4 +53,38 @@ public class Server {
         }
         return  arrayList;
     }
+
+    /**
+     * main-loop function
+     * @throws IOException
+     */
+    private void run() throws IOException{
+        List<String> quotes = readFile();
+        Random random = new Random();
+
+        while (true) {
+            Socket clientSocket = this.acceptConnection();
+
+            DataOutputStream outputStream = null;
+            if(clientSocket != null) {
+                outputStream = new DataOutputStream(clientSocket.getOutputStream());
+            }
+            outputStream.writeUTF(quotes.get(random.nextInt(quotes.size())));
+            if(clientSocket != null) {
+                clientSocket.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if(args.length < 1) {
+            System.out.println("Usage: port");
+            System.exit(1);
+        }
+        int port = Integer.valueOf(args[0]);
+        Server server = new Server(port);
+        server.run();
+    }
+
+
 }
